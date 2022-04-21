576aa375b5eac0899262488868fed31950f55e1f
Nicole T <nicole.a.gregory@gmail.com>
2022-04-21 16:50:20 -0700

    for colorado on mac

--- challenge/charts.js
+++ challenge/charts.js
@@ -42,6 +42,8 @@ function buildMetadata(sample) {
 
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
+    // PANEL.append('h6').text(result.location);
+    // PANEL.append('h6').text(result.id);
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
@@ -57,32 +59,40 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
-    var alldata = data.metadata;
-    
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
 
+    console.log(data);
+    // 3. Create a variable that holds the samples array. 
+    var sampleArray = data.samples; 
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var sampleFilter = sampleArray.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
 
-    var results = resultArray[0];
+    var sampleResults = sampleFilter[0];
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otu = sample.otu_ids
+
+    var PANEL2 = d3.select('#sample-otu_ids');
+    var PANEL3 = d3.select('#sample-otu_labels');
+    var PANEL4 = d3.select('#sample-sample_values');
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = PANEL2.map(bacteria=>bacteria.PANEL2).slice(0,10).reverse()
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = {
+      x: PANEL4,
+      y: yticks,
+      type: "bar",
       
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     
     };
+
+    var trace = [barData];
+    // 9. Create the layout for the bar chart. 
+    // var barLayout = {
+     
+    // };
     // 10. Use Plotly to plot the data with the layout. 
     
   });
